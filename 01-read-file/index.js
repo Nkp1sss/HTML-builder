49099756f01b709b7e86548cd0dba333a1cb9410
Nikita <nikitakemper555@gmail.com>
2022-11-04 12:58:35 +0300

    feat: add task solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+// const readableStream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+
+let body = '';
+readableStream.on('data', chunk => body += chunk);
+readableStream.on('end', () => console.log(body));
+readableStream.on('error', error => console.log('Error', error.message));
