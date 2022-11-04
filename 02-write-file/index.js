09f9c3da6ab3a9a926013ff58728e71272da51ae
Nikita <nikitakemper555@gmail.com>
2022-11-04 13:00:43 +0300

    feat: add task solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const { stdin, stdout } = process;
+
+stdout.write('Привет, введите текст.\nЧтобы выйти, введите exit или нажми Ctrl + C\n');
+
+stdin.on('data', data => {
+    if (data.toString().trim() == 'exit')
+        process.exit();
+    output.write(data);
+});
+
+process.on('exit', () => stdout.write('Пока..'));
