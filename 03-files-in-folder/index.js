80825c8b5e458bd6a299acc22ad19c428a2a91c7
Nikita <nikitakemper555@gmail.com>
2022-11-05 17:42:40 +0300

    refactor: update algorithm

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const folder = 'secret-folder';
 const fullPath = path.join(__dirname, folder);
 
+/** С async/await */
 (async function readDir() {
     try {
         const files = await fs.readdir(fullPath);
@@ -20,36 +21,3 @@ const fullPath = path.join(__dirname, folder);
         console.error(err);
     }
 })()
-
-
-
-
-
-
-
-//////////////////////////////////
-/** Без промисов (все работает) */
-//////////////////////////////////
-
-// const fs = require('fs');
-// const path = require('path');
-
-// const folder = 'secret-folder';
-// const fullPath = path.join(__dirname, folder);
-
-// fs.readdir(fullPath, (error, files) => {
-//     if (error) console.log('Error', error.message);
-
-//     files.forEach(file => {
-//         fs.stat(path.join(fullPath, file), (error, stats) => {
-//             if (error) console.log('Error', error.message);
-
-//             if (stats.isFile()) {
-//                 const ext = path.extname(path.join(fullPath, file))
-//                 .slice(1);
-//                 console.log(`${file.split('.')[0]} - ${ext} - ${(stats.size / 1024)
-//                 .toFixed(3)}kb`);
-//             }
-//         })
-//     });
-// })
